0e7e162c4c45610a4ad0320cde170bff0cc7800c
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2025-02-08 12:47:10 +0400

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,34 +1,77 @@
-CREATE DATABASE LESSONSDB
+﻿CREATE DATABASE LessonsDB
 
-USE LESSONSDB
+USE LessonsDB
 
--- თეიბლის შექმნა
-CREATE TABLE PERSONS(
-FIRSTNAME VARCHAR(30),
-LASTNAME VARCHAR(40),
-ADDRES VARCHAR(50),
-)
+--თეიბლის შექმნა
+CREATE TABLE Persons1(
+PersonsID VARCHAR(50),
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+City VARCHAR(50),
+Addres VARCHAR(50)
+);
 
---თეიბლიდან ინფორმაციის ამოშლა
+----თეიბლიდან ინფორმაციის ამოშლა
 TRUNCATE TABLE PEROSNS
-  
---თეიბლში დამატება
-ALTER TABLE PERSONS
-ADD AGE VARCHAR(40);
 
---თეიბში სვეტის წაშლა
-ALTER TABLE PERSONS
-DROP COLUMN AGE;
+--თეიბლიდან სვეტის წაშლა
+ALTER TABLE Persons
+DROP COLUMN City;
 
+--თეიბლში სვეტის ჩასმა
+ALTER TABLE Persons
+ADD City VARCHAR(50);
 
-ALTER TABLE PERSONS
-ADD NAMELIST VARCHAR(40);
-
---სვეტის შეცვლა INT ტიპად
-ALTER TABLE PERSONS
-ALTER COLUMN FIRSTNAME int;
+ALTER TABLE Persons  
+ALTER COLUMN PersonsID BIGINT;
 
 
-ALTER TABLE PERSONS
-ALTER COLUMN LASTNAME INT;
+
+--თეიბლში მონაცემის გაზრდა
+ALTER TABLE Persons
+ALTER COLUMN PersonsID varchar(50);
+
+--თეიბლში ინფორმაციის შენახვა
+INSERT INTO Persons1(
+PersonsID,
+FirstName,
+LastName,
+City,
+Addres) VALUES(
+1,
+'Juba',
+'Koguashvili',
+'Kutaisi',
+'Buxaidze'
+);
+
+INSERT INTO Persons1 VALUES(
+2,
+'Juba!',
+'Koguashvili!',
+'Kutaisi!',
+'Buxaidze!'
+);
+
+-- უნიკალური მნიშვნელობები
+ALTER TABLE Persons1
+ADD PersonsID INT IDENTITY (100,2) PRIMARY KEY
+
+INSERT INTO Persons1 VALUES(
+'JUBA',
+'KOGUASHVILI',
+'KUTAISI',
+'BUXAIDZE'),
+
+('SEBASTIAN',
+'BAXI',
+'VAKE',
+'CHAVCHAVADZE');
+
+
+
+
+
+
+
 
