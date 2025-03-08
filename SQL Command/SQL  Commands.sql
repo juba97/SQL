dbaa2a358a9dff144745d81c9437bd7ad690d0a8
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2025-03-08 18:17:14 +0400

    Add files via upload

---
+++ SQL Command/SQL  Commands.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE Exercises4
+USE Exercises4
+
+CREATE TABLE Employees(
+EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+FirstName VARCHAR(50),
+Lastname VARCHAR(50),
+Age INT CONSTRAINT chk CHECK (Age >= 18),
+Salary DECIMAL(10,2)
+)
+
+ALTER TABLE Employees
+ADD Department VARCHAR(50)
+
+ALTER TABLE Employees
+ALTER COLUMN Salary DECIMAL(10,2)
+
+ALTER TABLE Employees
+DROP CONSTRAINT chk
+
+ALTER TABLE Employees
+DROP COLUMN Age
+
+USE Exercises
+
+DROP DATABASE Exercises4
+
+CREATE TABLE Employees(
+EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+FirstName VARCHAR(50),
+Lastname VARCHAR(50),
+Salary INT,
+Department VARCHAR(20)
+)
+
+SELECT * FROM Employees
+
+INSERT INTO Employees(
+FirstName,
+Lastname,
+Salary,
+Department
+)VALUES(
+'Antonio',
+'Moreno',
+2000,
+'IT'
+)
+INSERT INTO Employees(
+FirstName,
+Lastname,
+Salary,
+Department
+)VALUES(
+'Thomas',
+'Hardy',
+2500,
+'Finance'
+)
+INSERT INTO Employees(
+FirstName,
+Lastname,
+Salary,
+Department
+)VALUES(
+'Christina',
+'Berglund',
+2200,
+'HR'
+)
+
+SELECT FirstName, LastName, department, Salary * 1.10 AS [Increased 10%]
+FROM Employees
+
+
+DELETE FROM Employees
+WHERE Salary < 2100;
+
+SELECT AVG(Salary) AS AVgSalary, STRING_AGG(FirstName, ',') AS Name
+FROM Employees
+
+SELECT MAX(Salary), STRING_AGG( Department, ',')
+FROM Employees
+
+SELECT 
+    MAX(Salary) AS MaxSalary, 
+    STRING_AGG(Department, ', ') AS Departments
+FROM Employees;
+
+SELECT Salary, Department
+FROM Employees
+GROUP BY Department, Salary
+HAVING AVG(Salary) > 2200
+
+SELECT Department, FirstName FROM Employees
+WHERE Department = 'HR'
+
+
