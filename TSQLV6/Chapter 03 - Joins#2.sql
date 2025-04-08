f9bfc854735aa9b632eac8cee0cabddf57d7280a
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2025-04-08 15:35:16 +0400

    Update Chapter 03 - Joins#2.sql

--- TSQLV6/Chapter 03 - Joins#2.sql
+++ TSQLV6/Chapter 03 - Joins#2.sql
@@ -49,8 +49,3 @@ SELECT C.custid, C.companyname, O.orderid, O.orderdate
 FROM Sales.Customers AS C  
 LEFT OUTER JOIN Sales.Orders AS O ON C.custid = O.custid
 WHERE O.orderdate >= '20220101'
-
-
-
-
-
