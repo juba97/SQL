bfea5a235bebf1d4ae01b131d11eed3792c7fcc0
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2025-04-18 19:45:39 +0400

    Update Chapter 05 - Table Expressions - VIEW.sql

--- TSQLV6/Chapter 05 - Table Expressions - VIEW.sql
+++ TSQLV6/Chapter 05 - Table Expressions - VIEW.sql
@@ -1,45 +1,45 @@
 CREATE OR ALTER VIEW Sales.USACusts 
 AS SELECT 
-		  custid,
-	      companyname, 
-		  contactname, 
-		  contacttitle, 
-		  address, 
-		  city, 
-		  region, 
-		  postalcode, 
-		  country, 
-		  phone, 
-		  fax
+	custid,
+	companyname, 
+	contactname, 
+	contacttitle, 
+	address, 
+	city, 
+	region, 
+	postalcode, 
+	country, 
+        phone, 
+	fax
 FROM Sales.Customers
 WHERE country = N'USA';
 GO
 
 SELECT 
-	   custid,
+       custid,
        companyname
 FROM Sales.USACusts;
 
 SELECT 
-		custid,
-		companyname,
-		region
+	custid,
+	companyname,
+	region
 FROM Sales.USACusts
 ORDER BY region;
 
 CREATE OR ALTER VIEW Sales.USACusts 
 AS SELECT TOP (100) PERCENT
 	    custid,
-		companyname, 
-		contactname,
-		contacttitle,
-		address,
-		city,
-		region,
-		postalcode,
-		country,
-		phone,
-		fax
+	    companyname, 
+	    contactname,
+	    contacttitle,
+	    address,
+	    city,
+	    region,
+	    postalcode,
+	    country,
+	    phone,
+	    fax
 FROM Sales.Customers
 WHERE country = N'USA'
 ORDER BY region;
@@ -48,40 +48,40 @@ GO
 CREATE OR ALTER VIEW Sales.USACusts 
 AS SELECT TOP (100) PERCENT
 		    custid, 
-			companyname, 
-			contactname, 
-			contacttitle, 
-			address,
-			city,
-			region,
-			postalcode,
-			country,
-			phone,
-			fax
+		    companyname, 
+		    contactname, 
+		    contacttitle, 
+		    address,
+		    city,
+		    region,
+		    postalcode,
+		    country,
+		    phone,
+		    fax
 FROM Sales.Customers
 WHERE country = N'USA'
 ORDER BY region;
 GO
 
 SELECT 
-	   custid,
-	   companyname,
-	   region
+	custid,
+	companyname,
+	region
 FROM Sales.USACusts;
 
 CREATE OR ALTER VIEW Sales.USACusts 
 AS SELECT 
-		custid,
-		companyname,
-		contactname,
-		contacttitle,
-		address,
-		city,
-		region,
-		postalcode,
-		country,
-		phone,
-		fax
+	custid,
+	companyname,
+	contactname,
+	contacttitle,
+	address,
+	city,
+	region,
+	postalcode,
+	country,
+	phone,
+	fax
 FROM Sales.Customers
 WHERE country = N'USA'
 ORDER BY region
@@ -90,9 +90,9 @@ GO
 
 CREATE OR ALTER VIEW Sales.USACusts
 AS SELECT 
-		custid,
-		companyname, contactname, contacttitle, address,
-		city, region, postalcode, country, phone, fax
+	custid,
+	companyname, contactname, contacttitle, address,
+	city, region, postalcode, country, phone, fax
 FROM Sales.Customers
 WHERE country = N'USA';
 GO
@@ -127,6 +127,3 @@ WHERE country = N'USA';
 GO
 
 EXEC sp_helptext 'Sales.USACusts';
-
-
-
