74401c72c5da03cd05c89091145cb50aefdd5a6f
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-05-06 02:00:13 +0400

    Delete Tasks-for-SQL/cat-facts directory

--- Tasks-for-SQL/cat-facts/public/js/factories/socket.factory.js
+++
@@ -1,8 +0,0 @@
-/* global angular, io */
-var app = angular.module('catfacts');
-
-app.factory('socket', ['socketFactory', '$location', function(socketFactory, $location) {
-    return socketFactory({
-        ioSocket: io.connect($location.$$absUrl + '/')
-    });
-}]);
--- Tasks-for-SQL/cat-facts/public/js/filters/capitalize.js
+++
@@ -1,6 +0,0 @@
-// https://stackoverflow.com/questions/30207272/capitalize-the-first-letter-of-string-in-angularjs
-angular.module('catfacts').filter('capitalize', function() {
-    return function(input) {
-      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
-    };
-});
