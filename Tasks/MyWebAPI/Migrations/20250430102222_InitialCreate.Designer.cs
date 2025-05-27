d78d87c97d4bdd9555c1c8508195a569d68bbc2f
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-05-27 22:04:09 +0400

    Update 20250430102222_InitialCreate.Designer.cs

--- Tasks/MyWebAPI/Migrations/20250430102222_InitialCreate.Designer.cs
+++ Tasks/MyWebAPI/Migrations/20250430102222_InitialCreate.Designer.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyWebAPI.Data;
-
 #nullable disable
 
 namespace MyWebAPI.Migrations
