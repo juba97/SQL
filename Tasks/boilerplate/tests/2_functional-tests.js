1dcaae24e61786b320da7d1fa3b6865d46de369c
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-05-31 21:51:38 +0400

    Node.js project with testing environment

--- Tasks/boilerplate/tests/2_functional-tests.js
+++ Tasks/boilerplate/tests/2_functional-tests.js
@@ -53,10 +53,28 @@ suite('Functional Tests', function () {
         'Colombo',
         'res.body.surname should be "Colombo"'
       );
+      
       done();
     });
   });
-});
+    
+ // #4
+    test('send {surname: "da Verrazzano"}', function(done) {
+  /** place the chai-http request code here... **/
+  chai
+    .request(server)
+    .put('/travellers')
+    .send({ surname: 'da Verrazzano' })
+    /** place your tests inside the callback **/
+    .end(function(err, res) {
+      assert.equal(res.status, 200, 'response status should be 200');
+      assert.equal(res.type, 'application/json', 'Response should be json');
+      assert.equal(res.body.name, 'Giovanni');
+      assert.equal(res.body.surname, 'da Verrazzano');
+      
+      done();
+    });
+  });
 
 const Browser = require('zombie');
 
