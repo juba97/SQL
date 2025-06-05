09cd1e48881923b58b6fbc148e9e22c964f47dbb
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-06-05 14:39:26 +0400

    Update server.js

--- Tasks/boilerplate-advancednode/server.js
+++ Tasks/boilerplate-advancednode/server.js
@@ -1,25 +1,71 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const { ObjectId } = require('mongodb');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+// Middleware setup
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Session setup
+app.use(session({
+  secret: process.env.SESSION_SECRET, // Ensure this exists in .env
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// View engine setup
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+// Database connection
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Routes
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in'
+    });
+  });
+
+  // Passport config
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+}).catch(e => {
+  console.error(e);
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Unable to connect to database',
+      message: e.message
+    });
+  });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Server listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
-
